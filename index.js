970fd6cd97e3e1580c2c6683a8d49eaf71c00e77
L1ghtee <nikorak20@gmail.com>
2023-08-16 17:37:55 +0300

    234

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const CryptoJS = require("crypto");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
